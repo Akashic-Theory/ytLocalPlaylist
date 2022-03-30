aa66834e76107e545d050d91ad51b8f55d00633a
MyUncle <myepicuncle@gmail.com>
2022-03-30 15:45:07 +0530

    :white_check_mark: Added Unit tests
    [SongMetaData]
    - Testing Happy Path and invalid tokens for yt id conversions

---
+++ tests/SongMetaDataTest.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Mayank on 3/29/2022.
+//
+#include <catch2/catch_test_macros.hpp>
+#include "../src/SongMetaData.h"
+
+TEST_CASE("SongMetaData id conversions are valid", "[metadata][SongMetaData]") {
+	SongMetaData dat{};
+	dat.id = 0;
+	SECTION("Happy Path") {
+		std::string tokens[] = {
+			"c61BuM8YzNg",
+			"JwoqQuixiV0",
+			"_VA6eSOO88M",
+			"kQQB29kQnqw",
+			"70ByjZkJX28",
+			"C76yS0OtJNA",
+			"Km_3HavZ6U0",
+			"Nhs_hDyZpwk",
+			"mC9MCTmbiXo",
+			"G2w_luBdrsA"
+		};
+		SECTION("Token conversion is reversible") {
+			for (const auto &token : tokens) {
+				INFO("Failed with token - " << token);
+				REQUIRE(dat.setID(token));
+				REQUIRE(dat.getToken() == token);
+			}
+		}
+	}
+	SECTION("setID invalid token returns false") {
+		SECTION("Token too short") {
+			REQUIRE_FALSE(dat.setID("TOOSHORT"));
+		}
+
+		SECTION("Token too long") {
+			REQUIRE_FALSE(dat.setID("TOKENMUCHTOOLONG"));
+		}
+
+		SECTION("Token contains invalid characters") {
+			REQUIRE_FALSE(dat.setID("~ !@#$%^&*+"));
+		}
+
+		SECTION("Final character contains incorrect subset") {
+			for (char c: "BCDFGHJKLNOPRSTVWXZabdefhijlmnpqrtuvxyz1235679-_") {
+				std::string base = "__________";
+				INFO("FAILED with character - " << c);
+				CHECK_FALSE(dat.setID(base + c));
+			}
+		}
+	}
+}
