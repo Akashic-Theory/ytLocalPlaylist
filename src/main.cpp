4512bdacb02e315401a3aa35848c78c28b7faa96
MyUncle <myepicuncle@gmail.com>
2022-07-26 18:25:25 -0500

    :heavy_plus_sign: Add TinyProcessLibrary

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "raylib.h"
 #include "ui/Button.h"
+#include "process.hpp"
+#include "nlohmann/json.hpp"
+
+namespace tinyproc = TinyProcessLib;
+using json = nlohmann::json;
 
 int main () {
+    SetTraceLogLevel(LOG_NONE);
     InitWindow(800, 600, "YouTube Playlist Manager");
     SetTargetFPS(60);
 
@@ -10,8 +16,11 @@ int main () {
     button.action([]() -> void {
         std::cout << GetTime() << std::endl;
     });
+    tinyproc::Process process("yt-dlp --version");
+    if (process.get_exit_status()) {
+        std::cout << "Not Found on path" << std::endl;
+    }
 
-    button();
     while (!WindowShouldClose()) {
         BeginDrawing();
         {
